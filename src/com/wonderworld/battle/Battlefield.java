abbae46216fc837b72769824aba596350b63410b
Lukas K <lukas-kannenberg@web.de>
2015-09-20 00:51:12 +0200

    Added Grammar

--- src/com/wonderworld/battle/Battlefield.java
+++ src/com/wonderworld/battle/Battlefield.java
@@ -6,7 +6,7 @@ public class Battlefield {
 		
 	}
 	
-	public void initiliseTeams(Pokemon[] myTeam, Pokemon[] oppTeam) {
+	public void initializeTeams(Pokemon[] myTeam, Pokemon[] oppTeam) {
 		
 	}
 	
