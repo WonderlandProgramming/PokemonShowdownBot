b431fb4468710e78fa516aaa94930f760268b8bb
Lukas K <lukas-kannenberg@web.de>
2015-09-20 23:50:31 +0200

    Added: - parts of pickIntelligentStarter(), main calculations still need to be done; the method assigns each Pokemon a value depending on how good it serves as a lead, then randomly a Pokemon will be chosen however, the higher he number of that Pokemon is, the higher is the chance it gets picked

--- src/com/wonderland/battle/ai/BattleAI.java
+++ src/com/wonderland/battle/ai/BattleAI.java
@@ -1,5 +1,7 @@
 package com.wonderland.battle.ai;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 import com.wonderland.battle.BattlePokemon;
@@ -9,13 +11,60 @@ public class BattleAI {
 	
 	Battlefield battlefield = new Battlefield();
 	
+	/**
+	 * Constructs a new BattleAI.
+	 * 
+	 * @param battlefield
+	 */
 	public BattleAI(Battlefield battlefield) {
 		this.battlefield = new Battlefield();
 	}
 	
+	/**
+	 * @return a number representing a random starter Pokemon
+	 */
 	public int pickRandomStarter() {
-		BattlePokemon[] pokemon = battlefield.getMyTeam().toArray(new BattlePokemon[0]);
-		int random = new Random().nextInt(pokemon.length);
-		return random++;
+		BattlePokemon[] pokemons = battlefield.getMyTeam().toArray(new BattlePokemon[0]);
+		if(pokemons.length != 0) {
+			int random = new Random().nextInt(pokemons.length);
+			return random++;
+		}
+		return -1;
+	}
+	
+	/**
+	 * @return a number representing an intelligent starter Pokemon
+	 */
+	public int pickIntelligentStarter() {
+		BattlePokemon[] pokemons = battlefield.getMyTeam().toArray(new BattlePokemon[0]);
+		int[] values = new int[pokemons.length];
+		if (pokemons.length != 0) {
+			return -1;
+		}
+		// Calculate and add best Pokemon for any matchup (max. 100)
+		// TODO
+		
+		// Calculate if Hazard/FakeOut can be applied (max. 25)
+		// TODO
+		
+		// Calculate if enemy has Hazard/FakeOut that can be countered (max. 25)
+		// TODO
+		
+		// Calculate if weather start has impact on own/enemy team if available
+		// TODO
+		int sum = 0;
+		for (int i = 0; i < values.length; i++) {
+			int value = values[i];
+			sum += value;
+			values[i] = sum + value;
+		}
+		Arrays.sort(values);
+		int random = new Random().nextInt(sum);
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] <= random) {
+				return i++;
+			}
+		}
+		return -1;
 	}
 }
